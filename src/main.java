04807cfc32e0848fafd09062ae09e0c9937e2bdb
Ethan <41381482+ethandudu@users.noreply.github.com>
2023-11-15 12:52:27 +0100

    Added main.java template

---
+++ src/main.java
@@ -0,0 +1,11 @@
+import java.io.FileInputStream;
+import java.util.Scanner;
+import java.util.Arraylist;
+
+public class main {
+
+}
+
+public static void main(String[] args) throws Exception {
+
+}
