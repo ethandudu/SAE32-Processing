676a696290d858bfd41b51346e15a6934d44a668
YanisBkd <yanisbk08@gmail.com>
2023-11-29 11:01:44 +0100

    Mise à jour de code

--- src/main/java/rt/sae32/processing/Main.java
+++ src/main/java/rt/sae32/processing/Main.java
@@ -2,15 +2,34 @@ package rt.sae32.processing;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.FileReader;
+
 public class Main {
     public static void main(String[] args) {
-        String fichierDuplique = "~/Documents/GitHub/wireshark.json";
-        String fichierUtilise = "~/Documents/GitHub/wireshark_reel.json";
-        RemoveDuplicateKeys.main(fichierDuplique, fichierUtilise);
-        
-        JSONArray fichier = new JSONArray(fichierUtilise);
-        for (int i = 0; i < fichier.length(); i++) {
+        try {
+            // Spécifiez le chemin du fichier JSON avec les clés dupliquées
+            String fichierDuplique = "/nas2/users/etudiant/b/by310239/Documents/GitHub/wireshark.json";
             
+            // Spécifiez le chemin du fichier JSON sans les clés dupliquées
+            String fichierUtilise = "/nas2/users/etudiant/b/by310239/Documents/GitHub/wireshark_modifie.json";
+            
+            // Fonction qui supprime les clés dupliquées
+            RemoveDuplicateKeys.main(fichierDuplique, fichierUtilise);
+            
+            // Créez un objet FileReader
+            FileReader lireFichier = new FileReader(fichierUtilise);
+
+            // 
+            JSONArray fichierArray = new JSONArray(fichierUtilise);
+
+        for (int i = 0; i < fichierArray.length(); i++) {
+            JSONObject objetATrouver = fichierArray.getJSONObject(i);
+            System.out.println("IP Source : " + objetATrouver.get("ip.src"));
+        }
+
+            
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
