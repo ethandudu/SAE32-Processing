9659a0a0f4314860ee5553bf414ed224e49a70f6
YanisBkd <yanisbk08@gmail.com>
2023-12-08 11:07:05 +0100

    Début de traitement du fichier JSON

--- src/main/java/rt/sae32/processing/Main.java
+++ src/main/java/rt/sae32/processing/Main.java
@@ -1,7 +1,10 @@
 package rt.sae32.processing;
+
+import org.apache.commons.io.FileUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.File;
 import java.io.FileReader;
 
 public class Main {
@@ -17,17 +20,28 @@ public class Main {
             RemoveDuplicateKeys.main(fichierDuplique, fichierUtilise);
             
             // Créez un objet FileReader
-            FileReader lireFichier = new FileReader(fichierUtilise);
+            //FileReader lireFichier = new FileReader(fichierUtilise);
 
             // 
-            JSONArray fichierArray = new JSONArray(fichierUtilise);
-
-        for (int i = 0; i < fichierArray.length(); i++) {
-            JSONObject objetATrouver = fichierArray.getJSONObject(i);
-            System.out.println("IP Source : " + objetATrouver.get("ip.src"));
-        }
-
             
+
+            String json = FileUtils.readFileToString(new File(fichierUtilise), "UTF-8");
+            JSONArray array = new JSONArray(json);
+
+            // JSONArray fichierArray = new JSONArray(fichierUtilise);
+
+            for (int i = 0; i < array.length(); i++) {
+                JSONObject objetATrouver = array.getJSONObject(i);
+                //System.out.println("Source : " + sourceATrouver.get("_source"));
+                //search in the object for the value of the key [_source][layers][frame][frame.protocols]
+                JSONObject source = objetATrouver.getJSONObject("_source");
+                JSONObject layers = source.getJSONObject("layers");
+                JSONObject frame = layers.getJSONObject("frame");
+                String protocols = frame.getString("frame.protocols");
+                System.out.println("Protocols : " + protocols);
+            }
+
+
         } catch (Exception e) {
             e.printStackTrace();
         }
