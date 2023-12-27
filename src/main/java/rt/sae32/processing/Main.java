b009742ecfe1dbb18e98bd0ece38d9f5107ad26c
Ethan <41381482+ethandudu@users.noreply.github.com>
2023-12-27 02:08:54 +0100

    Fixed response code handling Added dns support

--- src/main/java/rt/sae32/processing/Main.java
+++ src/main/java/rt/sae32/processing/Main.java
@@ -98,10 +98,92 @@ public class Main {
                             }
                         }
                     }
-                    /*
-                    if (protocols.getString(j).equals("DNS")) {
+
+                    if (protocols.getString(j).equals("dns")) {
+                        JSONObject dns = new JSONObject();
+                        JSONObject dnsFlags = layers.getJSONObject("dns").getJSONObject("dns.flags_tree");
+
+                        //Query or response
+                        String type = switch (dnsFlags.getString("dns.flags.response")) {
+                            case "0" -> "Query";
+                            case "1" -> "Response";
+                            default -> "Unknown";
+                        };
+                        System.out.println(type);
+                        JSONObject queries = layers.getJSONObject("dns").getJSONObject("Queries");
+                        JSONObject queriesTree = new JSONObject();
+
+
+                        for (String key : queries.keySet()){
+                            JSONObject query = queries.getJSONObject(key);
+                            String name = query.getString("dns.qry.name");
+                            String typeQuery = switch (query.getString("dns.qry.type")){
+                                case "1" -> "A";
+                                case "2" -> "NS";
+                                case "5" -> "CNAME";
+                                case "6" -> "SOA";
+                                case "15" -> "MX";
+                                case "16" -> "TXT";
+                                case "28" -> "AAAA";
+                                default -> query.getString("dns.qry.type");
+                            };
+                            queriesTree.put("name", name).put("type", typeQuery);
+                        }
+
+                        if (type.equals("Response")){
+                            JSONObject answersTree = new JSONObject();
+                            //check if it's a response or a authority
+                            if (layers.getJSONObject("dns").has("Answers")) {
+                                JSONObject answers = layers.getJSONObject("dns").getJSONObject("Answers");
+
+
+                                for (String key : answers.keySet()) {
+                                    JSONObject answer = answers.getJSONObject(key);
+                                    String name = answer.getString("dns.resp.name");
+                                    String typeAnswer = switch (answer.getString("dns.resp.type")) {
+                                        case "1" -> "A";
+                                        case "2" -> "NS";
+                                        case "5" -> "CNAME";
+                                        case "6" -> "SOA";
+                                        case "15" -> "MX";
+                                        case "16" -> "TXT";
+                                        case "28" -> "AAAA";
+                                        default -> answer.getString("dns.resp.type");
+                                    };
+                                    if (typeAnswer.equals("A")) {
+                                        answersTree.put("name", name).put("type", typeAnswer).put("ip", answer.getString("dns.a"));
+                                    } else if (typeAnswer.equals("AAAA")) {
+                                        answersTree.put("name", name).put("type", typeAnswer).put("ip", answer.getString("dns.aaaa"));
+                                    }
+                                }
+                                data.put("dns", dns.put("type", type).put("queries", queriesTree).put("answers", answersTree));
+                            } else if (layers.getJSONObject("dns").has("Authoritative nameservers")){
+                                JSONObject answers = layers.getJSONObject("dns").getJSONObject("Authoritative nameservers");
+
+                                for (String key : answers.keySet()) {
+                                    String name = answers.getJSONObject(key).getString("dns.resp.name");
+                                    String typeAnswer = switch (answers.getJSONObject(key).getString("dns.resp.type")) {
+                                        case "1" -> "A";
+                                        case "2" -> "NS";
+                                        case "5" -> "CNAME";
+                                        case "6" -> "SOA";
+                                        case "15" -> "MX";
+                                        case "16" -> "TXT";
+                                        case "28" -> "AAAA";
+                                        default -> answers.getJSONObject(key).getString("dns.resp.type");
+                                    };
+                                    String mname = answers.getJSONObject(key).getString("dns.soa.mname");
+                                    String rname = answers.getJSONObject(key).getString("dns.soa.rname");
+                                    answersTree.put("name", name).put("type", typeAnswer).put("mname", mname).put("rname", rname);
+                                }
+                            }
+                            data.put("dns", dns.put("type", type).put("queries", queriesTree).put("answers", answersTree));
+                        } else {
+                            data.put("dns", dns.put("type", type).put("queries", queriesTree));
+                        }
 
                     }
+                    /*
                     if (protocols.getString(j).equals("http")) {}
                     if (protocols.getString(j).equals("MPLS")) {}
                     if (protocols.getString(j).equals("TLS")) {}
@@ -149,23 +231,17 @@ public class Main {
             // switch on HTTP response code
             switch (SendData(Index.createIndexPacket(object.length(), testName), packet))
             {
-                case 200:
-                    System.out.println("Data inserted");
+                case 200 -> System.out.println("Data inserted");
 
-                case 410:
-                    System.out.println("No data provided");
+                case 410 -> System.out.println("No data provided");
 
-                case 411:
-                    System.out.println("No index provided");
+                case 411 -> System.out.println("No index provided");
 
-                case 412:
-                    System.out.println("No packets data provided");
+                case 412 -> System.out.println("No packets data provided");
 
-                case 400:
-                    System.out.println("Not Authorized");
+                case 400 -> System.out.println("Not Authorized");
 
-                case 504:
-                    System.out.println("Server did not respond");
+                case 504 -> System.out.println("Server did not respond");
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -198,10 +274,9 @@ public class Main {
         if (response == null)
             return 504; // server did not respond
 
-        System.out.println(response);
         JSONObject responseJson = new JSONObject(response);
 
-        return Integer.parseInt(responseJson.getString("responsecode"));
+        return (responseJson.getInt("responsecode"));
     }
 
     /**
